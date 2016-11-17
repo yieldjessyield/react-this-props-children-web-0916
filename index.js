819ecec3e9109fea50adf804470ca103f7c06640
Jess <jess.harrelson0707@gmail.com>
2016-11-16 20:30:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+// export default class Panel extends React.Component {
+//   render() {
+//     return (
+//       <div className="panel">
+//         <div className="panel-header">{this.props.title}</div>
+//         <div className="panel-body">{this.props.children}</div>
+//       </div>
+//     );
+//   }
+// }
